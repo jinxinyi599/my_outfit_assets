a29b8be813e5f9cf8eafef86dbad72f380458623
jinxinyi599 <q5r82vnb95@privaterelay.appleid.com>
2026-01-09 15:55:16 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,9 @@
+/**
+ * 👗 服装数据库
+ * img: 只需要写文件名，例如 'h01.webp'
+ */
+const CLOTH_DATABASE = [
+    { id: 'd01', name: '梦想行者', cat: 'top', star: 2, tags: [ ], img: 'd01.png' },
+    { id: 'd02', name: '酷夏黑星', cat: 'top', star: 2, tags: [ ], img: 'd02.png' },
+    // 在这里继续添加...
+];
