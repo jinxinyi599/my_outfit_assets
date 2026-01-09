328ffe3c0b812ec230fecb93a76cccdf65879619
jinxinyi599 <q5r82vnb95@privaterelay.appleid.com>
2026-01-09 22:08:42 +0800

    Add files via upload

--- data.js
+++ data.js
@@ -3,10 +3,12 @@
  * img: 只需要写文件名，例如 'h01.webp'
  */
 const CLOTH_DATABASE = [
+{ id: 'top200001', name: '梦想行者', cat: 'top', star: 2, tags: [ ], img: 'top200001.png' },
+{ id: 'top200002', name: '酷夏黑星', cat: 'top', star: 2, tags: [ ], img: 'top200002.png' },
 { id: 'hair200001', name: '日落舞曲', cat: 'hair', star: 2, tags: [ ], img: 'hair200001.png' },
 { id: 'hair200002', name: '十秒丸子头', cat: 'hair', star: 2, tags: [ ], img: 'hair200002.png' },
 { id: 'hair200003', name: '银灰羽絮', cat: 'hair', star: 2, tags: [ ], img: 'hair200003.png' },
 { id: 'hair200004', name: '满分优等生', cat: 'hair', star: 2, tags: [ ], img: 'hair200004.png' },
-{ id: 'dr200001', name: '雅静深绿', cat: 'dress', star: 2, tags: [ ], img: 'top200001.png' },
-{ id: 'dr200002', name: '月色迷离', cat: 'dress', star: 2, tags: [ ], img: 'top200002.png' }
+{ id: 'dr200001', name: '雅静深绿', cat: 'dress', star: 2, tags: [ ], img: 'dr200001.png' },
+{ id: 'dr200002', name: '月色迷离', cat: 'dress', star: 2, tags: [ ], img: 'dr200002.png' }
 ];
